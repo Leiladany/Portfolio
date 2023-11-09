e3ce6efe5561a63145cf5c17a506608742652ec3
Leiladany <leiladany16@gmail.com>
2023-11-09 23:27:21 +0000

    Adding packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Circle from './Circle'; // Import your Circle component
+
+const App = () => {
+  return (
+    <div>
+      <Circle />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Circle.jsx
@@ -0,0 +1,15 @@
+const Circle = () => {
+  const circleStyle = {
+    width: "541px",
+    height: "338px",
+    position: 'relative',
+    top: '100px',
+    left: '200px',
+    borderRadius: "540%",
+    backgroundColor: "#FCE1E4",
+  };
+
+  return <div style={circleStyle}></div>;
+};
+
+export default Circle;
