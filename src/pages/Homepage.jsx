92fbcf93a7ca1c61885965ab33181f76814abaef
Leiladany <leiladany16@gmail.com>
2023-11-16 19:42:37 +0000

    Add button, and add CSS

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,42 @@
 const Homepage = () => {
   return (
-    <div className="bigBox">
-      <div className="MyServices">
-        <h1>My Services</h1>
-        <div className="button-container">
-          <section>
-          <img src="/public/Images/wdicon.png" alt="Web Development"/>
-          <p>Web Development</p></section>
-          <section>Web Design</section>
-          <section>Video Editing</section>
+    <div>
+      <div className="bigBox">
+        <div className="MyServices">
+          <h1>My Services</h1>
+          <div className="button-container">
+            <section>
+              <img src="/Images/wdicon.png" alt="Web Development" />
+              <p>Web Development</p>
+            </section>
+            <section>
+              <img src="/Images/wdeicon.png" alt="Web Design" />
+              <p>Web Design</p>
+            </section>
+            <section>
+              <img src="/Images/veicon.png" alt="Video Editing" />
+              <p>Video Editing</p>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div className="bigBox">
+        <div className="Projects">
+          <h1>Projects</h1>
+          <div className="button-container">
+            <button>
+              <img src="/public/Images/gamepad-icon.png" alt="Web Development" />
+              <p>Games</p>
+            </button>
+            <button>
+              <img src="/Images/wdeicon.png" alt="Web Design" />
+              <p>Online Agenda</p>
+            </button>
+            <button>
+              <img src="/Images/veicon.png" alt="Video Editing" />
+              <p>Blog</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
