64f1b006f4f7ddeffcf6c2fa71cd26492bffda3c
Leiladany <leiladany16@gmail.com>
2023-12-18 19:22:52 +0000

    adding information on games

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { Link as RouterLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
-  
+
       const myServicesOffset = document.getElementById("my-services-section").offsetTop;
       const projectsOffset = document.getElementById("projects-section").offsetTop;
       const certificationsOffset = document.getElementById("certifications-section").offsetTop;
       const getInTouchOffset = document.getElementById("get-in-touch-section").offsetTop;
-  
+
       if (scrollPosition >= myServicesOffset && scrollPosition < projectsOffset) {
         setActiveSection("my-services-section");
       } else if (scrollPosition >= projectsOffset && scrollPosition < certificationsOffset) {
@@ -23,14 +26,13 @@ const Navbar = () => {
         setActiveSection("get-in-touch-section");
       }
     };
-  
+
     window.addEventListener("scroll", handleScroll);
-  
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
 
   const scrollToSection = (sectionId) => {
     scroll.scrollTo(document.getElementById(sectionId).offsetTop, {
@@ -41,73 +43,42 @@ const Navbar = () => {
     });
   };
 
-  const getNavbarColor = () => {
-    switch (activeSection) {
-      case "my-services-section":
-        return "#dfc9ff";
-      case "projects-section":
-        return "#e8dff5";
-      case "certifications-section":
-        return "#dfc9ff";
-      case "get-in-touch-section":
-        return "#e8dff5";
-      default:
-        return "#dfc9ff";
-    }
-  };
-
   return (
-    <nav className="navbar" style={{ backgroundColor: getNavbarColor() }}>
+    <nav className="navbar">
       <div className="navbar-container">
         <div className="nav-menu">
           <i className="fas fa-bars"></i>
         </div>
 
         <ul className="nav-menu">
-          <li className={`nav-item ${activeSection === "my-services-section" ? "active" : ""}`}>
-            <ScrollLink
-              to="my-services-section"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              My Services
-            </ScrollLink>
-          </li>
-          <li className={`nav-item ${activeSection === "projects-section" ? "active" : ""}`}>
-            <ScrollLink
-              to="projects-section"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Projects
-            </ScrollLink>
-          </li>
-          <li className={`nav-item ${activeSection === "certifications-section" ? "active" : ""}`}>
-            <ScrollLink
-              to="certifications-section"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Certifications
-            </ScrollLink>
-          </li>
-          <li className={`nav-item ${activeSection === "get-in-touch-section" ? "active" : ""}`}>
-            <ScrollLink
-              to="get-in-touch-section"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Get In Touch
-            </ScrollLink>
-          </li>
+          {location.pathname === "/games" ? (
+            <li className="nav-item">
+              <RouterLink to="/homepage">Go back to Homepage</RouterLink>
+            </li>
+          ) : (
+            <>
+              <li className={`nav-item ${activeSection === "my-services-section" ? "active" : ""}`}>
+                <ScrollLink to="my-services-section" spy={true} smooth={true} offset={0} duration={500}>
+                  My Services
+                </ScrollLink>
+              </li>
+              <li className={`nav-item ${activeSection === "projects-section" ? "active" : ""}`}>
+                <ScrollLink to="projects-section" spy={true} smooth={true} offset={0} duration={500}>
+                  Projects
+                </ScrollLink>
+              </li>
+              <li className={`nav-item ${activeSection === "certifications-section" ? "active" : ""}`}>
+                <ScrollLink to="certifications-section" spy={true} smooth={true} offset={0} duration={500}>
+                  Certifications
+                </ScrollLink>
+              </li>
+              <li className={`nav-item ${activeSection === "get-in-touch-section" ? "active" : ""}`}>
+                <ScrollLink to="get-in-touch-section" spy={true} smooth={true} offset={0} duration={500}>
+                  Get In Touch
+                </ScrollLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,13 +1,31 @@
+// Game.jsx
 import React from "react";
 
 const Game = () => {
   return (
     <div className="game-container">
       <div className="game">
-        <img src="game1.jpg" alt="Game 1" />
+        <img src="/public/Images/game-img1.png" alt="Game 1" />
         <div className="game-info">
-          <h3>Game 1</h3>
-          <p>Description for Game 1.</p>
+          <h3>Escaping-Westview</h3>
+          <p>
+            Step into the mesmerizing universe of 'Escaping Westview,' a 2D game
+            inspired by Marvel's 'WandaVision.' Your mission: dodge the
+            relentless spells cast by the cunning witch Agatha Harkness.
+            <br></br>
+            <br></br> Use your reflexes to maneuver up, down, and sideways,
+            navigating a visually stunning landscape reminiscent of the Disney+
+            series. <br></br> <br></br>Crafted with JavaScript (ES6), HTML,
+            Canvas, and CSS, this game seamlessly blends technology and magic.{" "}
+            <br></br>
+            <br></br>Your goal is simple yet challenging: avoid Agatha's spells
+            for as long as possible. The tension builds with each successful
+            dodge until you are inevitably hit, revealing your final spell
+            count. <br></br>
+            <br></br>Immerse yourself in 'Escaping Westview' and experience the
+            thrill of skillful evasion in a captivating, bite-sized gaming
+            adventure. How long can you outsmart Agatha's magic?
+          </p>
         </div>
       </div>
     </div>
